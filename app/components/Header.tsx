2f82ac65bf00130c65b4332024c4e62c64d5216f
Evrlink <evrlinkmeeps@gmail.com>
2026-04-25 17:59:11 -0700

    feat: add Connect Wallet button to header web only

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,24 +1,43 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useAccount } from "wagmi";
+import { useMiniKit } from "@coinbase/onchainkit/minikit";
+import WalletPickerModal from "./WalletPickerModal";
 
 export default function Header() {
+  const { isConnected } = useAccount();
+  const { context, isFrameReady } = useMiniKit();
+  const [showPicker, setShowPicker] = useState(false);
+
   return (
-    <header className="flex items-center justify-center px-4 pt-2.5 pb-0.5">
-      {/* Evrlink Logo */}
-      <div className="flex items-center">
-        <Link href="/">
-        <Image
-          src="/images/logo.png"
-          alt="Evrlink"
-          width={60}
-          height={60}
-          className="h-[50px] w-[50px] object-contain"
-          priority
-        />
-        </Link>
-      </div>
-    </header>
+    <>
+      <header className="relative flex items-center justify-center px-4 pt-2.5 pb-0.5">
+        {/* Evrlink Logo */}
+        <div className="flex items-center">
+          <Link href="/">
+          <Image
+            src="/images/logo.png"
+            alt="Evrlink"
+            width={60}
+            height={60}
+            className="h-[50px] w-[50px] object-contain"
+            priority
+          />
+          </Link>
+        </div>
+        {isFrameReady && context === null && !isConnected && (
+          <button
+            className="absolute right-4 rounded-lg bg-[#00B2C7] px-3 py-2 text-xs font-semibold text-white"
+            onClick={() => setShowPicker(true)}
+          >
+            Connect Wallet
+          </button>
+        )}
+      </header>
+      <WalletPickerModal isOpen={showPicker} onClose={() => setShowPicker(false)} />
+    </>
   );
 }
